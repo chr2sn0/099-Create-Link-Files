531bc6d8e39726f347486c7c3cc6a8faf575817c
Christian Nowak <christian.nowak@galeria.de>
2023-02-22 18:03:17 +0100

    test the lambda way

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import pathlib
 import os
 
-for pdp_file in pathlib.Path('./data/__groupfolders/1/').glob('**/*.pdp'):
-    print(pdp_file)
+the_dir = './data/__groupfolders/1/'
+all_txt_files = filter(lambda x: x.endswith('.pdp'), os.listdir(the_dir))
+print(all_txt_files)
+
+# import pathlib
+# import os
+#
+# for pdp_file in pathlib.Path('./data/__groupfolders/1/').glob('**/*.pdp'):
+#    print(pdp_file)
