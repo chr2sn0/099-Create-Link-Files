fad8735a75bbf98fba8bddffaf129f1f74031c05
Christian Nowak <christian.nowak@galeria.de>
2023-02-22 16:25:26 +0100

    remove testcode

--- main.py
+++ main.py
@@ -3,8 +3,3 @@ import os
 
 for pdp_file in pathlib.Path('./data/__groupfolders/1/').glob('**/*.pdp'):
     print(pdp_file)
-
-for file in os.listdir("./data"):
-    print(file)
-
-# do something with "txt_file"
